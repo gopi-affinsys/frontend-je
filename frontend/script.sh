64002cbff03c6664d1bc34430b04a49aea140c24
gopi-affinsys <gopi.krishna@affinsys.com>
2025-04-14 11:58:39 +0530

    Update script.sh

--- frontend/script.sh
+++ frontend/script.sh
@@ -5,7 +5,7 @@ FILE="report.txt"
 low=$(grep  "LOW" "$FILE" | wc -l)
 medium=$(grep  "MEDIUM" "$FILE" | wc -l)
 high=$(grep  "HIGH" "$FILE" | wc -l)
-critical=$(grep  "CIRITCAL" "$FILE" | wc -l)
+critical=$(grep  "CRITICAL" "$FILE" | wc -l)
 unknown=$(grep "UNKNOWN" "$FILE" | wc -l)
 
 echo "Vulnerability Severity Count:"
