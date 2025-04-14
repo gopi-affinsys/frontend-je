76fcc70799267b781505724ebd5e560044f840fd
gopi-affinsys <gopi.krishna@affinsys.com>
2025-04-14 12:01:56 +0530

    Update script.sh

--- frontend/script.sh
+++ frontend/script.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
-
-FILE="report.txt"
-
-low=$(grep  "LOW" "$FILE" | wc -l)
-medium=$(grep  "MEDIUM" "$FILE" | wc -l)
-high=$(grep  "HIGH" "$FILE" | wc -l)
-critical=$(grep  "CRITICAL" "$FILE" | wc -l)
-unknown=$(grep "UNKNOWN" "$FILE" | wc -l)
-
-echo "Vulnerability Severity Count:"
-echo "Low:      $low"
-echo "Medium:   $medium"
-echo "High:     $high"
-echo "Critical: $critical"
+  
+  FILE="report.txt"
+  
+  export low=0
+  export medium=0
+  export high=0
+  export critical=0
+  export unknown=0
+  
+  low=$(grep  "LOW" "$FILE" | wc -l)
+  medium=$(grep  "MEDIUM" "$FILE" | wc -l)
+  high=$(grep  "HIGH" "$FILE" | wc -l)
+  critical=$(grep  "CRITICAL" "$FILE" | wc -l)
+  unknown=$(grep "UNKNOWN" "$FILE" | wc -l)
+  
+  echo "Vulnerability Severity Count:"
+  echo "Low:      $low"
+  echo "Medium:   $medium"
+  echo "High:     $high"
+  echo "Critical: $critical"
