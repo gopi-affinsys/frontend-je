6eab8ec12cdd5ff1390f9cb140acadd8a4cfee23
gopi-affinsys <gopi.krishna@affinsys.com>
2025-04-14 12:01:43 +0530

    Update values.sh

--- values.sh
+++ values.sh
@@ -1,21 +1,21 @@
-#!/bin/bash
-
-FILE="report.txt"
-
-low=0
-medium=0
-high=0
-critical=0
-unknown=0
-
-low=$(grep  "LOW" "$FILE" | wc -l)
-medium=$(grep  "MEDIUM" "$FILE" | wc -l)
-high=$(grep  "HIGH" "$FILE" | wc -l)
-critical=$(grep  "CRITICAL" "$FILE" | wc -l)
-unknown=$(grep "UNKNOWN" "$FILE" | wc -l)
-
-echo "Vulnerability Severity Count:"
-echo "Low:      $low"
-echo "Medium:   $medium"
-echo "High:     $high"
-echo "Critical: $critical"
+  #!/bin/bash
+  
+  FILE="report.txt"
+  
+  export low=0
+  export medium=0
+  export high=0
+  export critical=0
+  export unknown=0
+  
+  low=$(grep  "LOW" "$FILE" | wc -l)
+  medium=$(grep  "MEDIUM" "$FILE" | wc -l)
+  high=$(grep  "HIGH" "$FILE" | wc -l)
+  critical=$(grep  "CRITICAL" "$FILE" | wc -l)
+  unknown=$(grep "UNKNOWN" "$FILE" | wc -l)
+  
+  echo "Vulnerability Severity Count:"
+  echo "Low:      $low"
+  echo "Medium:   $medium"
+  echo "High:     $high"
+  echo "Critical: $critical"
